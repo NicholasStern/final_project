f7288da83c1f320be5ae57f0bb068bb3cc683d10
Nicholas Stern <nicholas_stern@g.harvard.edu>
2018-12-17 22:27:18 -0500

    pushing so julien can code state generator function

--- Linear_Q_agent/run.py
+++ Linear_Q_agent/run.py
@@ -3,23 +3,32 @@ import numpy as np
 from approxQ import ApproxQ
 from collections import Counter
 
-def gen_data(path, window_size):
-    # read in history
+def gen_states(path, window_size, history_size):
+    # read in data
     df = pd.read_csv(path)
     df = df[['Open', 'High', 'Low', 'Close']].values
 
     # segment into time windows
-    df_split = np.array_split(df, df.shape[0]//window_size)
-
-    # fix the size of the first window if split is uneven
-    df_split[0] = df_split[0][len(df_split[0])-window_size:]
+    df_split = np.array_split(df[:df.shape[0] // window_size * window_size], df.shape[0] // window_size)
 
     # split into train/val/test (80%, 10%, 10%)
     train = df_split[:int(.8 * len(df_split))]
     val = df_split[int(.8* len(df_split)):int(.9 * len(df_split))]
     test = df_split[int(.9 * len(df_split)):]
 
+    # important check
+    data = [train, val, test]
+    for dataset in data:
+        for w in dataset:
+            assert len(w) == window_size
+
+    # generate states
+    for i in range(history_size-1, len(train)*window_size):
+
+
+
     return train, val, test
+
 
 def evaluate(title, actions, regret):
     ac = Counter(actions)
@@ -29,8 +38,9 @@ def evaluate(title, actions, regret):
     print('{} avg. regret: {:.4f}'.format(title, np.mean(regret)))
 
 ## Initialize Parameters
-window_size = 5
-train, val, test = gen_data('../histories/apple.csv', window_size)
+window_size = 20  # days
+history_size = 3  # days
+train, val, test = gen_data('../histories/Apple_cleaned.csv', window_size)
 actions = ['buy', 'wait']
 epsilon = 0
 discount = 1
@@ -38,7 +48,7 @@ alpha = .1
 nfeats = 5
 
 ## Pass to Approximate Q-Learning Agent
-agent = ApproxQ('train', actions, train, epsilon, discount, alpha, nfeats)
+agent = ApproxQ('train', actions, train, epsilon, discount, alpha, nfeats, history_size)
 train_actions, train_regret = agent.learn()
 evaluate('train', train_actions, train_regret)
 
